50f43af36040f21e9cf11167fa2f26e78e4aef6b
mandeep-vratesh <mandeep.vratesh@gmail.com>
2017-03-29 12:00:26 +0530

    initial commit

--- app/src/main/java/com/example/mandeep/androidworkshop/ContactDetailsActivity.java
+++ app/src/main/java/com/example/mandeep/androidworkshop/ContactDetailsActivity.java
@@ -1,7 +1,16 @@
 package com.example.mandeep.androidworkshop;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by MANDEEP on 3/29/2017.
  */
-public class ContactDetailsActivity {
+public class ContactDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactdetails);
+    }
 }
